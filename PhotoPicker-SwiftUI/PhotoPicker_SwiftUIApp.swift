7a3fa12ac58672cae8de22a8e1bbf3e2221dd45c
jiajianhui <jia15176168273@icloud.com>
2024-01-01 20:21:54 +0800

    Initial Commit

---
+++ PhotoPicker-SwiftUI/PhotoPicker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPicker_SwiftUIApp.swift
+//  PhotoPicker-SwiftUI
+//
+//  Created by 贾建辉 on 2024/1/1.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPicker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
